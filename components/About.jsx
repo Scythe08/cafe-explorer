7b982f72a2fdada0a742cbe875c7601d34aeeb34
Scythe08 <michaeljustin.yalung@gsfe.tupcavite.edu.ph>
2022-12-16 18:33:16 +0800

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -9,7 +9,7 @@ const AboutUs = () => {
         <p>
         We are the Team Explorers and we made the webapp about online coffee ordering system. We aim to make an efficient
         way of sending your favorite blends of morning beverage with just easy steps right on your doorstep. We make all kinds of coffee blends from
-        authentic filipino originals to western blends at your own request. We hope you enjoy our services and don't forget to leave a feedback so that
+        authentic filipino originals to western blends at your own request. We hope you enjoy our services and {"don't"} forget to leave a feedback so that
         we can improve our skills. Thank you very much.               
         </p>
       </div>
