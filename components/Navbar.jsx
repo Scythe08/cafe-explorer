130a436c4a64919f772ee6cd50cbec685924c960
Scythe08 <michaeljustin.yalung@gsfe.tupcavite.edu.ph>
2022-12-16 20:04:11 +0800

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,9 +13,9 @@ const Navbar = () => {
 
       
       <div className={styles.navbarActions}>
-        
-        <Button variant="secondary"><Link href="/">Home</Link></Button>
-        
+        <Link href="/">
+        <Button variant="secondary">Home</Button>
+        </Link>
         <Link href="/menu">
         <Button variant="secondary">Menu</Button>
         </Link>
